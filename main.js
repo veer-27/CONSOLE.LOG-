32ebcc5382c75789bc1bbdde47c1d1213db290bc
veer-27 <86522744+veer-27@users.noreply.github.com>
2021-10-20 20:06:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+
+console.log("My First Console Project");
+
+var first_name = "Veer";
+
+var last_name = "Pratap";
+
+console.log(first_name.concat(last_name));
+
+var a = 15;
+
+var b = 5;
+
+var add = a + b;
+
+console.log(add);
